aecebbe11c306d1982b39d2c5ce0088dc08c115b
NMiller20 <nmiller20@stuy.edu>
2021-09-30 14:02:24 -0400

    classwork

---
+++ hw - semester 1/08_hellofxn/Greet2.java
@@ -0,0 +1,20 @@
+public class Greet2 {
+ 	public static void main(String[] args) {
+ 		Greetfxn("Gloria");
+ 		Greetfxn("Lauren");
+ 		Greetfxn("Nora");
+ 	}
+ 	
+ 	public static void Greetfxn(String name){
+ 		System.out.println("Hello, " + name + "! How're you doing?"); 
+ 	}
+}
+
+/** Discoveries/Questions:
+
+- cp can be used to copy files in the terminal
+- only static functions can be invoked from main???
+- it is indicated at the top of this window whether my file has been saveed or not
+
+**/
+
