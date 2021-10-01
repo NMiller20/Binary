68c1d6f460a1d7b88e171d616c9dc2cebff8fd9a
Nora Miller <nmiller20@stuy.edu>
2021-10-01 05:38:20 -0400

    new classwork

---
+++ hw - semester 1/08_greetfxn/Greet2.java
@@ -0,0 +1,11 @@
+public class Greet2 {
+   public static void main(String[] commandLine) {
+	   greet('Nora'); 
+	   greet('Lauren');
+	   greet('Gloria');
+  }
+  
+  public static void greet(String name) {
+	  System.out.println("Hi " + name + "! How're you doing?");
+  }
+}
