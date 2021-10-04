1c53606364d22f0763bb9cb52db09fad3d1163c8
Nora Miller <nmiller20@stuy.edu>
2021-10-03 23:01:59 -0400

    hw 9

---
+++ hw - semester 1/09_bigsib/BigSib.java
@@ -0,0 +1,21 @@
+/** 
+KEVIN
+(Nora, Hans, Neil, Hatch, Justin, ???)
+APCS
+HW 09 -- even more greetings
+2021-10-03
+*/
+
+public class BigSib {
+   public static void greet(String name) {
+	   System.out.println("Hi " + name + "! How're you doing?");
+  }
+
+}
+/** 
+DISCOVERIES: 
+- You can have a Java program invoke functions from java files within the same directory as itself
+
+UNRESOLVED QUESTIONS:
+(none)
+*/
