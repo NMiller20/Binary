679c24dbdff13aee61e1e266715d4b5a8baafc8e
Nora Miller <nmiller20@stuy.edu>
2021-10-06 00:30:37 -0400

    hw 10-5

---
+++ 1stSemester/12_bigsib/BigSib3.java
@@ -0,0 +1,30 @@
+/** 
+KEVIN
+(Nora, Hans, Neil, Hatch, Justin, Alfred)
+APCS
+HW 09 -- even more greetings
+2021-10-03
+*/
+
+public class BigSib3 {
+	public String HelloMsg = "";
+
+    public String greet(String name) {
+	   return(HelloMsg + ", " + name);
+  }
+  
+  
+    public void setHelloMsg(String message) {
+		HelloMsg = message;
+		
+	  
+  }
+
+}
+/** 
+DISCOVERIES: 
+- (see other file)
+
+UNRESOLVED QUESTIONS:
+(none)
+*/
