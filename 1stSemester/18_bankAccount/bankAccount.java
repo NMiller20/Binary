643d1e774547a6044a6d3c80bfceabee8d7bde13
Nora Miller <nmiller20@stuy.edu>
2021-10-17 21:56:59 -0400

    sun hw

--- 1stSemester/18_bankAccount/bankAccount.java
+++ 1stSemester/18_bankAccount/bankAccount.java
@@ -1,25 +1,48 @@
-/** public String holderName
-private String password
-private int pin
-private int accountNum
-private double balance */
+/** 
+Ocho
+(Nora, Tasmin, Hamim)
+APCS
+HW 18 -- writing a whole class
+2021-10-17
 
-public class bankAccount {
+DISCOVERIES: 
+ 
+
+UNRESOLVED QUESTIONS:
+- Am I allowed to name my directories something more helpful, provided that they have the same numbers as everyone else's?
+
+Q2: Given the definitions of what constructors and instances are, 
+the fact that you could create an new instance of a class by invoking a method which you have not yourself defined 
+indicates the existence of a default constructor. 
+
+Q3: 
+0. Create a variable representing your object
+1. Print your object to the terminal. 
+2. If what you see in the terminal is the same as what your object contains 
+(ex. your object is an integer, and you see an integer, or your object is a string array and you see a string array)
+then your object has a string representation. 
+
+
+*/
+
+public class BankAccount {
 	public String holderName;
 	private String password;
 	private int pin;
 	private int accountNum;
 	private double balance;
+	private static int numAccounts = 0;
 	
-	public bankAccount(String name, String pass, int pinn, int num) {
+	public BankAccount(String name, String pass, int pinn) {
 		holderName = name;
 		password = pass;
 		pin = pinn;
-		accountNum = num;
+		accountNum = numAccounts + 1;
+		numAccounts += 1; 
 		balance = 0;
 	}
 	
-	public void changePassword(String newPass) {
+	public void changePswd(String newPass) {
 		password = newPass;
 	}
 	
@@ -33,10 +56,29 @@ public class bankAccount {
 	
 	public void withdraw(double amount) {
 		balance -= amount;
-	}
+	} 
 	
 	public void atmReceipt() {
-		System.out.print(holderName + ": " + balance);
+		System.out.print("\n\nAccount " + accountNum + ": " + holderName + "\nPassword: " + password + "\nPin: " + pin + "\nBalance: " + balance);
+	}
+	
+	public static void main(String[] args){
+		BankAccount roberts = new BankAccount("Bob", "momsbday050667", 2048); // Robert's bank account - he goes by Bob
+		roberts.atmReceipt();
+		roberts.changePswd("dadsbday080663");
+		roberts.changePin(4096); 
+		roberts.deposit(500); 
+		roberts.atmReceipt(); 
+		
+		BankAccount mayas = new BankAccount("Maya", "momsbday041569", 2048); 
+		mayas.atmReceipt();
+		mayas.deposit(400); 
+		mayas.withdraw(200); 
+		mayas.atmReceipt(); 
+		
+		System.out.println(roberts);
+		
 	}
 	
 }
+
