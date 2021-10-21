3a6e814926f88f1838be5bd4c4d59b1919421b77
Nora Miller <nmiller20@stuy.edu>
2021-10-21 08:33:50 -0400

    finally succeeded

--- 1stSemester/21_stats/StatsDriver.java
+++ 1stSemester/21_stats/StatsDriver.java
@@ -1,26 +1,72 @@
 public class StatsDriver {
 	// test mean: 
-	System.out.println(mean(4, 6));	
-	System.out.println(mean(4.5, 6.5) );
-	System.out.println("Expected: 5 and 5.5"); 
-	System.out.println(mean(0, 2);	// 1
-	System.out.println(mean(1, 1) ); // 1
-	System.out.println(mean(-5, -7));	// -6
-	System.out.println(mean(-5, 3) ); // -1
-	System.out.println(mean(1, 2));	// does this convert to doubles, or does it perform the operation under the assumption that 
-								    // these are integers, then fail? or does it round?
-	System.out.println(mean(0, 6.5) ); //
-	System.out.println(mean(-4.4, 6.5) );
-	System.out.println(mean(-4.4, -6.5) );
+	public static void main(String[] args){
+		System.out.println(Stats.mean(4, 6));	
+		
+		
+		System.out.println(Stats.mean(4.5, 6.5) );
+		System.out.println("Expected: 5 and 5.5"); 
+		System.out.println(Stats.mean(0, 2));	// 1
+		System.out.println(Stats.mean(1, 1) ); // 1
+		System.out.println(Stats.mean(-5, -7));	// -6
+		System.out.println(Stats.mean(-5, 3) ); // -1
+		System.out.println(Stats.mean(1, 2));	// does this convert to doubles, or does it perform the operation under the assumption that 
+										// these are integers, then fail? or does it round?
+		System.out.println(Stats.mean(0, 6.5) ); // 3.25
+		System.out.println(Stats.mean(-4.4, 6.5) ); //1.05
+		System.out.println(Stats.mean(-4.4, -6.5) ); //-5.45
+		System.out.println(Stats.mean(4.5, 6) ); //5.25
+		
+		// success
+		
+		
+		System.out.println("\nMax function"); 
+		
+		System.out.println(Stats.max(5, 7)); //7
+		System.out.println(Stats.max(7.5, 9.3)); //9.3 Since the double and int versions are written in the exact same way, these cases
+												 // only need to be tested with one. 
+		System.out.println(Stats.max(4.4, 4.4) ); //4.4
+		System.out.println(Stats.max(4.4, -6.5) ); //4.4
+		System.out.println(Stats.max(-4.4, -6.5) ); //-4.4
+		System.out.println(Stats.max(4.4, 0) ); //4.4
+		
+		System.out.println(Stats.max(5, 7, 6) ); //7
+		System.out.println(Stats.max(5, 6, 7) ); //7
+		System.out.println(Stats.max(7, 5, 6) ); //7
+		System.out.println(Stats.max(0, 7.8, 6.9) ); //7.8
+		System.out.println(Stats.max(5, 7.8, 62) ); //62.0
+		System.out.println(Stats.max(-5.8, -7, 6) ); //6.0
+		System.out.println(Stats.max(-5, -7, -6) ); //-5
+		
+		
+		System.out.println("\nGeo Mean function"); 
+		
+		
+		System.out.println(Stats.geoMean(4, 9)); //6
+		System.out.println(Stats.geoMean(12.25, 42.25) ); //22.75
+		 
+		
+		System.out.println(Stats.geoMean(12.25, 0) ); //0
+		System.out.println(Stats.geoMean(-12.25, 42.25) ); // error
+		System.out.println(Stats.geoMean(-12.25, -42.25) ); // 22.75
+		System.out.println(Stats.geoMean(12.25, 12.25) ); // 12.25
+		System.out.println(Stats.geoMean(12.25, 9) ); // 10.5
+		
+		System.out.println(Stats.geoMean(12.25, 12.25, 12.25) ); // 12.25
+		System.out.println(Stats.geoMean(6, 6, 6) ); // 6
+		System.out.println(Stats.geoMean(1.0, 3.0, 9.0) ); // 3
+		System.out.println(Stats.geoMean(-1, -3, -9) ); // -3
+		System.out.println(Stats.geoMean(-1, -3, 9) ); // 3
+		System.out.println(Stats.geoMean(-1, 3, 9) ); // -3
+		System.out.println(Stats.geoMean(0, 3, 9) ); // 0	 
+		
+		
+
+		
+		
+		
+		
+		
+	}
 	
-	
-	
-	
-	System.out.println(max(5, 7));
-	System.out.println(max(7.5, 9.3));
-	System.out.println("Expected: 7, 9.3"); 
-	
-	System.out.println(geoMean(4, 9));
-	System.out.println(geoMean(12.25, 42.25) );
-	System.out.println("Expected: 6, 22.75" ); 
 }
