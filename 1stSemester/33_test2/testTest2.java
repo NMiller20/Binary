316d5cc2b43905e144dc4fb82e5b9a26a15954e1
Nora Miller <nmiller20@stuy.edu>
2021-11-22 07:56:55 -0500

    more old hw

---
+++ 1stSemester/33_test2/testTest2.java
@@ -0,0 +1,11 @@
+public class testTest2 {
+	public static void main(String[] args){
+		double x = 5; 
+		double y = 2; 
+		int m = (int)(x + y + x/y - x*y - x/(10*y) ); 
+		System.out.println(m); 
+		int n = (int)(3.7/1.2); 
+		System.out.println(n); 
+		System.out.println(6%4); 
+	}
+}
