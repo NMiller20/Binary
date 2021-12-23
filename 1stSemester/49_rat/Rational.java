6c1f15007dcf47ab7824dcac57f0dbb69d27a611
Nora Miller <nmiller20@stuy.edu>
2021-12-22 23:03:14 -0500

    longer than expected

--- 1stSemester/49_rat/Rational.java
+++ 1stSemester/49_rat/Rational.java
@@ -1,23 +1,28 @@
 /******************************
-
-
  // 3 Lucky Duckies: Gloria Lee, Ziying Jian, Nora Miller
  // APCS pd8
- // HW42 -- Be More Rational
- // 2021-12-07
+ // HW49 -- Rational Standards Compliance: getting something to match an interface and a superclass
+ // 2021-12-22
 
- Time Spent: 1hr
+ Time Spent: 1.2hr
 
  DISCO:
+ * Since Comparable is an interface, it cannot be declared as an object type
+ * instanceof - return t/f for whether or not an object is actually part of that class
+ * Even though you know the variable type is a Rational Java doesn't. Typecasting explicity
+ tells Java that the declared variable type is a Rational, or that one class is part of the other
+ * After compiling, we get notes that Driver.java uses or overrides a deprecated API.
 
- * Because we are changing rational values directly, we have to be careful when deciding what the expected outcome of following arithmetic will be.
-
- * We should use this.instVar whenever possible for clarity. It is also better to avoid using *= or anything similar for clarity.
-
-
- QCC: What are other methods that we can add to enrich Rational.java?
+ QCC:
+ * What are other methods that we can add to enrich Rational.java?
+ * (Why) would a try-catch loop not be acceptable here?
+* When would throwing an ClassCastException or any exception be useful for us?
+ * Did the instructions mean for us to write a method called equals with the same
+   signature as the built in one? because we ran it with the build in one, and as
+   mentioned in class previously, it seems to have just checked that all instance
+   variables were the same, since it said that two fractions with the same value
+   were not equal
  ******************************/
-import java.util.ClassCastExecption; 
 
 public class Rational implements Comparable {
 
@@ -52,14 +57,6 @@ public class Rational implements Comparable {
     }
 
 
-/*
-multiply
-takes 1 Rational object as a parameter and multiplies it by this Rational object
-does not return any value
-should modify this object and leave the parameter alone (see below for example)
-need not reduce the fraction
-*/
-
     public void multiply(Rational a) {
         this.numerator = this.numerator * a.numerator; // this.numerator is the numerator of the instance of Rational invoking this method
         this.denominator = this.denominator * a.denominator;
@@ -80,6 +77,7 @@ need not reduce the fraction
         this.numerator = this.numerator + rat.numerator;
         this.denominator = commonDenom; // denominator is set to common multiple
         rat.numerator = reset; // resets parameter object to its original value
+
     }
 
 
@@ -112,8 +110,13 @@ need not reduce the fraction
 
     }
 
+/*
+================================================================================
+=============THE ONLY REALLY IMPORTANT PART OF THE HW BEGINS HERE===============
+================================================================================
+*/
     public int compareTo(Object rat){ // should be a rational; we have to deal with it separately if it isn't
-      if (rat instanceof Rational) {
+      if (rat instanceof  Rational) {
         Rational workingRat = (Rational)(rat);
         Rational diff = new Rational(this.numerator, this.denominator);
         diff.subtract(workingRat);
@@ -125,109 +128,21 @@ need not reduce the fraction
           return -1;
         }
       } else {
-        throw new ClassCastExecption("\ncompareTo() input not a Rational");
+        ClassCastException notARat = new ClassCastException("\ncompareTo() input not a Rational");
+        throw notARat;
       }
-
-
-     // should be some variant of subtraction
-
    }
-      // since typecasting just chops off the decimal
 
+   public boolean equals(Object rat){ //
+     if (rat instanceof Rational) {
+       Rational workingRat = (Rational)(rat);
+       if (workingRat.floatVal() == this.floatVal()){
+         return true;
+       } return false;
+     } else {
+       ClassCastException notARat = new ClassCastException("\nequals() input not a Rational");
+       throw notARat;
+     }
+   }
 
-
-
-
-
-
-    public static void main(String[] args) {
-
-        Rational a = new Rational();
-        Rational r = new Rational(2, 3);
-        Rational s = new Rational(1,2);
-
-
-        System.out.println( "//============testing constructors===========" );
-        System.out.println( "Default constructor: " + a + "...expecting 0/1");
-        System.out.println("Testing overwritten constructor error message:");
-        Rational t = new Rational(3,0);
-        System.out.println( "t: "+ t + "...expecting 3/1");
-        System.out.println("\n");
-
-
-    /* ~~============================================================================================~~ */
-    /* ~~============================================================================================~~ */
-    /* ~~================================= PREVIOUS TEST CASES ======================================~~ */
-    /* ~~============================================================================================~~ */
-    /* ~~============================================================================================~~ */
-
-        System.out.println( "//============testing multiply/divide methods===========" );
-        System.out.println(r + "...expected 2/3");
-        System.out.println(s + "...expected 1/2");
-        System.out.println("\n");
-        r.multiply(s);
-        System.out.println("AFTER MULTIPLYING....");
-        System.out.println(r + "...expected 2/6");
-        System.out.println(s + "...expected 1/2");
-        System.out.println("\n");
-        System.out.println( "//============testing float point values===========" );
-        System.out.println("float value of r " + r.floatVal() + "...should be 0.3333");
-        System.out.println("float value of s: " + s.floatVal() + "...should be 0.5" );
-
-        r.multiply(a);
-        System.out.println("AFTER MULTIPLYING AGAIN....");
-        System.out.println(r + "...expected 0/6");
-        System.out.println(a + "...expected 0/1");
-        System.out.println("\n");
-
-        t.divide(s);
-        System.out.println("AFTER DIVIDING....");
-        System.out.println("t is: " + t + "...expecting 6/1");
-        System.out.println("s is: " + s + "...expecting 1/2");
-        System.out.println("\n");
-
-
-
-        /* ~~============================================================================================~~ */
-        /* ~~============================================================================================~~ */
-        /* ~~~~~~~~========================== NEW TEST CASES ===================================~~~~~~~~~~ */
-        /* ~~============================================================================================~~ */
-        /* ~~============================================================================================~~ */
-
-        //resetting rationals to original values
-        r = new Rational(2, 3);
-        s = new Rational(1,2);
-        t = new Rational(3,0);
-
-        System.out.println( "//============testing add/subtract methods===========" );
-        System.out.println("r is " + r); //
-        System.out.println("s is " + s);
-        r.add(s); // 4/6 + 1/2 = 7/6
-        System.out.println( r + " addition...expected 7/6" );
-        r.subtract(s); //expecting 7/6-1/2 = 8/12numerator
-        System.out.println(r + " subtraction... expected 8/12");
-        System.out.println("\n");
-
-        // TESTING THE ERROR
-        System.out.println("s is " + s);
-        System.out.println("t is " + t);
-        s.add(t); // 1/2 + 3/1 = 7/2
-        System.out.println( s + " addition...expected 7/2" );
-        s.subtract(t); //expecting 7/2 - 3/1 = 1/2
-        System.out.println( s + " subtraction... expected 1/2" );
-
-        System.out.println( "//============testing reduce method===========" );
-        System.out.println("r is " + r); //
-        r.reduce();
-        System.out.println("After reduction... r = " + r + " expected 2/3");
-        System.out.println("t is " + t);
-        t.reduce();
-        System.out.println("After reduction... t = " + t + " expected 3/1");
-
-        // Testing compareTo();
-
-        System.out.println(r.compareTo(s) + " expected 1"); // expected 1
-        System.out.println(s.compareTo(r) + " expected -1"); // expected -1
-        System.out.println(r.compareTo(r) + " expected 0"); // expected 0
-    }
 }//end class
